68e6958647a066f39b7ed8a8700da7185c6081a5
DivyanshuIn <divyanshu@thepabloproject.io>
2020-02-27 15:34:09 +0530

    tester

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WebCamComponent } from 'ack-angular-webcam';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'webcam-tester';
+
+  options:{
+    audio:false,
+    video:false,
+    width:300,
+    height:300,
+  }
+
+  base64:any=null;
+  constructor(){}
+
+  genBase64( webcam:WebCamComponent ){
+    webcam.getBase64()
+    .then( base=>this.base64=base)
+    .catch( e=>console.error(e) )
+  }
+
+  onCamError(err){
+
+    console.log(err)
+  }
+ 
+  onCamSuccess(res){
+    console.log('camera sucess');
+    console.log(res);
+  }
 }
